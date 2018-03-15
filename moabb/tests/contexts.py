018ddb279f2d18aa9bd98538cff0d7653ebbf523
Vinay Jayaram <vjayaram@danube.is.localnet>
2018-03-15 15:07:21 +0100

    Merge of pipeline updates. Probably will break. Lost analysis

--- moabb/tests/contexts.py
+++ moabb/tests/contexts.py
@@ -1,21 +1,20 @@
-from moabb.contexts import evaluations as ev
+from moabb.evaluations import evaluations as ev
 from moabb.datasets.bnci import BNCI2014001
-from moabb.viz import Results
-from moabb.contexts.motor_imagery import LeftRightImagery
+from moabb.analysis import Results
+from moabb.paradigms.motor_imagery import LeftRightImagery
 import unittest
 
 from pyriemann.spatialfilters import CSP
 from pyriemann.estimation import Covariances
 from sklearn.pipeline import make_pipeline
-import os
 
 from collections import OrderedDict
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 
 pipelines = OrderedDict()
 pipelines['C'] = make_pipeline(Covariances('oas'), CSP(8), LDA())
-d = BNCI2014001()
-d.selected_events = {k: d.event_id[k] for k in ['left_hand', 'right_hand']}
+dataset = BNCI2014001()
+
 
 class Test_CrossSess(unittest.TestCase):
     '''This is actually integration testing but I don't know how to do this
@@ -24,28 +23,27 @@ class Test_CrossSess(unittest.TestCase):
     run it. Putting this on the future docket...
 
     '''
-    def tearDown(self):
-        if os.path.isfile('results.hd5'):
-            os.remove('results.hd5')
 
     def return_eval(self):
-        return ev.CrossSessionEvaluation()
+        return ev.CrossSessionEvaluation(paradigm=LeftRightImagery(),
+                                         datasets=[dataset])
 
     def test_eval_results(self):
         e = self.return_eval()
-        r = Results(e,'results.hd5')
-        p = LeftRightImagery(pipelines, e, [d])
-        e.preprocess_data(d,p)
-        r.add(e.evaluate(d, 1,
-                         pipelines, p))
+        e.preprocess_data(dataset)
+        e.evaluate(dataset, 1, pipelines)
+
 
 class Test_CrossSubj(Test_CrossSess):
     def return_eval(self):
-        return ev.CrossSubjectEvaluation()
+        return ev.CrossSubjectEvaluation(paradigm=LeftRightImagery(),
+                                         datasets=[dataset])
+
 
 class Test_WithinSess(Test_CrossSess):
     def return_eval(self):
-        return ev.WithinSessionEvaluation()
+        return ev.WithinSessionEvaluation(paradigm=LeftRightImagery(),
+                                          datasets=[dataset])
 
 
 if __name__ == "__main__":
